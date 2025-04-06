b8b47ad198a373b6eeea838b9e110bb2be61431b
Veries Seals <veries@me.com>
2025-04-06 11:07:04 -0500

    Created a blank space in terminal for readabililty

--- roller_coaster_height_calculator.py
+++ roller_coaster_height_calculator.py
@@ -1,21 +1,33 @@
 # Roller Coaster Height Calculator
 # ---------------------------------------------
 
+# Created blank space for terminal readablility
+# ---------------------------------------------
+term_blank_space = "\n"
+print(term_blank_space)
+
+
 # Print the title of the Calculator
 # ---------------------------------------------
 
 print("Welcome to the Roller Coaster Height Calculator")
 
+term_blank_space = "\n"
+print(term_blank_space)
+
 # Create variable and input for end user to specify height
 # ---------------------------------------------
 
-height = int(input("What is your hight in cm"))
+height = int(input("What is your hight in cm "))
 
+
+term_blank_space = "\n"
+print(term_blank_space)
 
 # Create if/else condition to evaluate height 
 # ---------------------------------------------
 
-if height > 120:
+if height >= 120:
     print("You can ride the roller coaster")
 else:
     print("Sorry you have to grow taller before you can ride")
