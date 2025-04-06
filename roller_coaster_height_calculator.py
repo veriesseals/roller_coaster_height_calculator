881adb8548fce697d3e3930edabcf2f166084c1c
Veries Seals <veries@me.com>
2025-04-06 10:52:38 -0500

    Added welcome using print function

---
+++ roller_coaster_height_calculator.py
@@ -0,0 +1,9 @@
+# Roller Coaster Height Calculator
+# ---------------------------------------------
+
+# Print the title of the Calculator
+# ---------------------------------------------
+
+print("Welcome to the Roller Coaster Height Calculator")
+
+
