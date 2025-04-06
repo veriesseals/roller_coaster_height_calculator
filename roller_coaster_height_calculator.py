098175d54cce88c4ba15359fef421a44eacfbdfe
Veries Seals <veries@me.com>
2025-04-06 11:00:36 -0500

    Added height variable and assigned it to an input to intake height in cm. Also Created if/else conditional statement to validate height

--- roller_coaster_height_calculator.py
+++ roller_coaster_height_calculator.py
@@ -6,4 +6,16 @@
 
 print("Welcome to the Roller Coaster Height Calculator")
 
+# Create variable and input for end user to specify height
+# ---------------------------------------------
 
+height = int(input("What is your hight in cm"))
+
+
+# Create if/else condition to evaluate height 
+# ---------------------------------------------
+
+if height > 120:
+    print("You can ride the roller coaster")
+else:
+    print("Sorry you have to grow taller before you can ride")
