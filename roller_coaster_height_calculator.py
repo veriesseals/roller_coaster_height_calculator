53010b1e9aeb74703bcc55a03276a575a28f70e0
Veries Seals <veries@me.com>
2025-04-14 08:49:42 -0500

    Added photo option and price

--- roller_coaster_height_calculator.py
+++ roller_coaster_height_calculator.py
@@ -1,7 +1,7 @@
 # Roller Coaster Height Calculator
 # ---------------------------------------------
 
-# Created blank space for terminal readablility
+# Created blank space for terminal readability
 # ---------------------------------------------
 term_blank_space = "\n"
 print(term_blank_space)
@@ -19,6 +19,8 @@ print(term_blank_space)
 # ---------------------------------------------
 
 height = int(input("What is your hight in cm "))
+
+bill = 0
 
 
 term_blank_space = "\n"
@@ -38,11 +40,41 @@ if height >= 120:
     # Nested if/else statements
     # ---------------------------------------------
     if age >= 18:
+        bill = 12
         print("You are an adult give me $12!")
     elif age > 12 < 18:
+        bill = 7
         print("You are young, so you can play me $7")
     else:
-        print("You get the childrens price, I guess you can play me $5")
+        bill = 5
+        print("You get the children's price, I guess you can play me $5")
+
+    # Would you like to add a photo
+    # ---------------------------------------------
+    
+    term_blank_space = "\n"
+    print(term_blank_space)
+
+    print("How about buying a photo today?")
+    
+    term_blank_space = "\n"
+    print(term_blank_space)
+
+    would_you_like_photo = input("Would you like a photo? (Enter: y or n) ")
+    print(would_you_like_photo)
+
+    if would_you_like_photo == "y":
+        print("Please pay $3: ")
+        bill += 3 # Add $3 to the bill or total. Or bill = bill + 3
+        print(f"Your total bill is: ${bill}")
+    elif would_you_like_photo == "n":
+        print("Thank you, no charge: ")
+    else:
+        print("Thank you for your visit")
+
+    
+    # ---------------------------------------------
+
 else:
     print(f"You are {height} cm tall. There is a minimum of 120cm")
     print("Sorry you have to grow taller before you can ride")
